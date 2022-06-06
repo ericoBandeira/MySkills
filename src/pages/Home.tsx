6d4ddaafb541693937da263f5b446d9e3b02524e
Érico Maximiano Bandeira <ericobandeira@hotmail.com>
2022-06-06 18:38:08 -0300

    Add typescript and delete skills

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,36 @@
 import React, {useState, useEffect} from 'react';
-import {Text, StyleSheet, View, TextInput, FlatList} from 'react-native';
+import {
+  Text,
+  StyleSheet,
+  View,
+  TextInput,
+  FlatList,
+  Platform,
+} from 'react-native';
 import {Button} from '../components/Button';
 import {SkillCard} from '../components/SkillCard';
 
+interface SkillData {
+  id: string;
+  name: string;
+}
+
 export function Home() {
   const [newSkill, setNewSkill] = useState('');
-  const [mySkills, setMySkills] = useState([]);
+  const [mySkills, setMySkills] = useState<SkillData[]>([]);
   const [gretting, setGretting] = useState('');
 
   function handleAddNewSkill() {
-    setMySkills(oldState => [...oldState, newSkill]);
+    const data = {
+      id: String(new Date().getTime()),
+      name: newSkill,
+    };
+
+    setMySkills(oldState => [...oldState, data]);
+  }
+
+  function handleRemoveSkill(id: string) {
+    setMySkills(oldState => oldState.filter(skill => skill.id !== id));
   }
 
   useEffect(() => {
@@ -37,14 +58,19 @@ export function Home() {
           onChangeText={setNewSkill}
         />
 
-        <Button onPress={handleAddNewSkill} />
+        <Button onPress={handleAddNewSkill} title="Add" />
 
         <Text style={[styles.title, {marginVertical: 50}]}>My Skills</Text>
 
         <FlatList
           data={mySkills}
-          keyExtractor={item => item}
-          renderItem={({item}) => <SkillCard skill={item} />}
+          keyExtractor={item => item.id}
+          renderItem={({item}) => (
+            <SkillCard
+              skill={item.name}
+              onPress={() => handleRemoveSkill(item.id)}
+            />
+          )}
         />
       </View>
     </>
